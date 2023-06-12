2e7bc3f188e6a55f1350b8ae55d5c15cfbfdb4f4
T R Methu <fmethubd@gmail.com>
2023-06-12 12:18:20 +0600

    safe push

--- index.js
+++ index.js
@@ -106,8 +106,7 @@ async function run() {
 
         app.put("/classs/:id", async (req, res) => {
             const id = req.params.id;
-            const feedback = req.body;
-            console.log(feedback);
+            // console.log(feedback);
             // console.log('Id hitting to approve', req.params.id);
             const filter = {_id: new ObjectId(id)}
             const options = {upsert: true}
